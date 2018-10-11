c72e8114fafa1162a0a3260f9ff966d0a53970d4
Juliano Mesquita dos Santos <julianophp@gmail.com>
2018-10-11 18:31:20 -0400

    Update SchoolSystemTransferError.php

--- src/Error/SchoolSystemTransferError.php
+++ src/Error/SchoolSystemTransferError.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Mindgeek\Error;
+namespace School\Error;
 
 use Exception;
 
 /**
  * Class SchoolSystemTransferError
- * @package Mindgeek\Error
+ * @package School\Error
  */
 class SchoolSystemTransferError extends Exception
 {
