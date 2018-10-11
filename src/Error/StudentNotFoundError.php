b41e02784996ada89311762317e7f32886e66016
Juliano Mesquita dos Santos <julianophp@gmail.com>
2018-10-11 18:22:06 -0400

    Update StudentNotFoundError.php

--- src/Error/StudentNotFoundError.php
+++ src/Error/StudentNotFoundError.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Mindgeek\Error;
+namespace School\Error;
 
 use Exception;
 
 /**
  * Class StudentNotFoundError
- * @package Mindgeek\Error
+ * @package School\Error
  */
 class StudentNotFoundError extends Exception
 {
